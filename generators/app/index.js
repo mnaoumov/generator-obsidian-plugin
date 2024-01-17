36f9d369f1818c287dc17e55d4995183721b24b4
Michael Naumov <mnaoumov@gmail.com>
2024-01-16 21:41:15 -0600

    Lint

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,21 +1,21 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
 
 module.exports = class extends Generator {
   prompting() {
     this.log(
       yosay(
-        `Welcome to the ${chalk.red('generator-obsidian-plugin')} generator!`
+        `Welcome to the ${chalk.red("generator-obsidian-plugin")} generator!`
       )
     );
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
+        type: "confirm",
+        name: "someAnswer",
+        message: "Would you like to enable this option?",
         default: true
       }
     ];
@@ -28,23 +28,25 @@ module.exports = class extends Generator {
 
   writing() {
     this.fs.copyTpl(
-      this.templatePath('.'),
-      this.destinationPath('.'), {
-      currentYear: 1,
-      authorName: "TODO authorName",
-      pluginId: "TODO pluginId",
-      pluginName: "TODO pluginName",
-      pluginDescription: "TODO pluginDescription",
-      authorGitHubName: "TODO authorGitHubName",
-      isDesktopOnly: true,
-      pluginPurpose: "TODO pluginPurpose",
-      pluginClassName: "TODO"
-    }, null, {
-      globOptions: { dot: true }
-    }
+      this.templatePath("."),
+      this.destinationPath("."),
+      {
+        currentYear: 1,
+        authorName: "TODO authorName",
+        pluginId: "TODO pluginId",
+        pluginName: "TODO pluginName",
+        pluginDescription: "TODO pluginDescription",
+        authorGitHubName: "TODO authorGitHubName",
+        isDesktopOnly: true,
+        pluginPurpose: "TODO pluginPurpose",
+        pluginClassName: "TODO"
+      },
+      null,
+      {
+        globOptions: { dot: true }
+      }
     );
   }
 
-  install() {
-  }
+  install() {}
 };
