ba8ec43bde87c1e9cb152e57353b983850590ba2
Michael Naumov <mnaoumov@gmail.com>
2024-01-16 23:41:38 -0600

    Add onLayoutReady

--- generators/app/templates/src/pluginClassName.ts
+++ generators/app/templates/src/pluginClassName.ts
@@ -1,4 +1,10 @@
 import { Plugin } from "obsidian";
 
 export default class <%= pluginClassName %> extends Plugin {
+  public onload(): void {
+    this.app.workspace.onLayoutReady(this.onLayoutReady.bind(this));
+  }
+
+  private onLayoutReady(): void {
+  }
 }
