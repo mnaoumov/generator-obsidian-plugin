0db7f7dc2e7e5bc19df026a1a24d6130cb3a7d2f
Michael Naumov <mnaoumov@gmail.com>
2024-07-04 16:56:11 -0600

    Rewrite generator

--- generators/app/index.ts
+++ generators/app/index.ts
@@ -2,47 +2,28 @@ import Generator from "yeoman-generator";
 import yosay from "yosay";
 import type { PromptQuestions } from "../../node_modules/yeoman-generator/dist/questions.d.ts";
 import semver from "semver";
+import { readdir } from "fs/promises";
+import { join } from "path";
 import _chalk from "chalk";
 const chalk = _chalk as unknown as typeof _chalk.default;
 
-const minimumNodeVersion = "18.0.0"
+const minimumNodeVersion = "18.0.0";
 if (!semver.satisfies(process.version, `>=${minimumNodeVersion}`)) {
   console.error(`You need Node.js version ${minimumNodeVersion} or higher to use this generator.`);
   process.exit(1);
 }
 
-
-function makePluginName(pluginId: string): string {
-  return extractWords(pluginId).join(" ");
-}
-
-function extractWords(pluginId: string): string[] {
-  return pluginId.split("-").map(toPascalCase);
-}
-
-function toPascalCase(word: string): string {
-  return (word[0] ?? "").toUpperCase() + word.substring(1);
-}
-
-function makePluginClassName(pluginId: string): string {
-  return extractWords(pluginId).concat("Plugin").join("");
-}
-
-function nameof<T>(name: Extract<keyof T, string>): string {
-  return name;
-}
-
-interface Answers {
+type Answers = {
   pluginId: string;
   pluginName: string;
+  pluginShortName: string;
   pluginDescription: string;
-  pluginClassName: string;
   currentYear: number;
   authorName: string;
   authorGitHubName: string;
   isDesktopOnly: boolean;
   hasStyles: boolean;
-}
+};
 
 export default class extends Generator {
   private answers!: Answers;
@@ -124,38 +105,18 @@ export default class extends Generator {
 
     this.answers = await this.prompt(questions);
     this.answers.currentYear = new Date().getFullYear();
-    this.answers.pluginClassName = makePluginClassName(this.answers.pluginId);
+    this.answers.pluginShortName = extractWords(this.answers.pluginId).join("");
   }
 
-  public writing(): void {
-    const map = new Map([
-      ["src/main.ts", ""],
-      ["src/pluginClassName.ts", `src/${this.answers.pluginClassName}.ts`],
-      [".editorconfig", ""],
-      [".gitattributes", ""],
-      [".gitignore.template", ".gitignore"],
-      [".hotreload", ""],
-      [".npmrc.template", ".npmrc"],
-      ["esbuild.config.ts", ""],
-      ["eslint.config.ts", ""],
-      ["eslint.config.fix.d.ts", ""],
-      ["LICENSE", ""],
-      ["manifest.json", ""],
-      ["package.json", ""],
-      ["README.md", ""],
-      ["styles.css", this.answers.hasStyles ? "" : null],
-      ["tsconfig.json", ""],
-      ["version-bump.ts", ""],
-      ["versions.json", ""],
-    ]);
+  public async writing(): Promise<void> {
+    const templatesDir = join(__dirname, "templates");
 
-    for (const [templatePath, mappedDestinationPath] of map) {
-      if (mappedDestinationPath === null) {
+    for await (const filePath of getAllFiles(templatesDir)) {
+      const templatePath = filePath.substring(templatesDir.length + 1);
+      const destinationPath = getDestinationPath(templatePath, this.answers);
+      if (!destinationPath) {
         continue;
       }
-
-      const destinationPath =
-        mappedDestinationPath === "" ? templatePath : mappedDestinationPath;
 
       this.fs.copyTpl(
         this.templatePath(templatePath),
@@ -163,5 +124,86 @@ export default class extends Generator {
         this.answers,
       );
     }
+
+    const packageJson = {
+      "name": this.answers.pluginId,
+      "version": "1.0.0",
+      "description": this.answers.pluginDescription,
+      "scripts": {
+        "build": "tsx scripts/npmScriptExecutor.ts build",
+        "dev": "tsx scripts/npmScriptExecutor.ts dev",
+        "lint": "tsx scripts/npmScriptExecutor.ts lint",
+        "postversion": "tsx scripts/npmScriptExecutor.ts postversion",
+        "preversion": "tsx scripts/npmScriptExecutor.ts preversion",
+        "version": "tsx scripts/npmScriptExecutor.ts version"
+      },
+      "keywords": [],
+      "author": this.answers.authorGitHubName,
+      "license": "MIT",
+      "devDependencies": {
+        "@stylistic/eslint-plugin": "^2.3.0",
+        "@tsconfig/strictest": "^2.0.5",
+        "@types/eslint": "^8.56.10",
+        "@types/node": "^20.14.9",
+        "@typescript-eslint/eslint-plugin": "^7.15.0",
+        "@typescript-eslint/parser": "^7.15.0",
+        "builtin-modules": "^4.0.0",
+        "esbuild": "^0.23.0",
+        "eslint": "^8.57.0",
+        "eslint-import-resolver-typescript": "^3.6.1",
+        "eslint-plugin-import": "^2.29.1",
+        "eslint-plugin-modules-newlines": "^0.0.7",
+        "globals": "^15.8.0",
+        "obsidian": "^1.5.7-1",
+        "obsidian-typings": "mnaoumov/obsidian-typings#main",
+        "tsx": "^4.16.2"
+      },
+      "type": "module"
+    };
+
+    this.fs.extendJSON(this.destinationPath("package.json"), packageJson);
   }
+}
+
+function makePluginName(pluginId: string): string {
+  return extractWords(pluginId).join(" ");
+}
+
+function extractWords(pluginId: string): string[] {
+  return pluginId.split("-").map(toPascalCase);
+}
+
+function toPascalCase(word: string): string {
+  return (word[0] ?? "").toUpperCase() + word.substring(1);
+}
+
+function nameof<T>(name: Extract<keyof T, string>): string {
+  return name;
+}
+
+async function* getAllFiles(dirPath: string): AsyncGenerator<string> {
+  const files = await readdir(dirPath, { withFileTypes: true });
+
+  for (const file of files) {
+    const filePath = join(dirPath, file.name);
+    if (file.isDirectory()) {
+      yield* getAllFiles(filePath);
+    } else {
+      yield filePath;
+    }
+  }
+}
+
+function getDestinationPath(templatePath: string, answers: Answers): string | null {
+  templatePath = templatePath.replace(/%= (.+?) %/g, (_: string, answerKey: keyof Answers) => String(answers[answerKey]));
+
+  if (templatePath.endsWith(".noext")) {
+    return templatePath.slice(0, -".noext".length);
+  }
+
+  if (templatePath === "styles.css" && !answers.hasStyles) {
+    return null;
+  }
+
+  return templatePath;
 }
