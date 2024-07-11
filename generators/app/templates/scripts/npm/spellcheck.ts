989d0ef6f59ac6b1d462de7f4508a4e4885c21e9
Michael Naumov <mnaoumov@gmail.com>
2024-07-11 04:30:01 -0600

    Lint

--- generators/app/templates/scripts/npm/spellcheck.ts
+++ generators/app/templates/scripts/npm/spellcheck.ts
@@ -1,5 +1,5 @@
 import { execFromRoot } from "../tools/root.ts";
 
-export default async function spellcheck(): Promise<void> {
+export default function spellcheck(): void {
   execFromRoot("npx cspell . --no-progress");
 }
