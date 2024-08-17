7d3f148efdef295108c942e850a81a68ee3e2949
Michael Naumov <mnaoumov@gmail.com>
2024-08-17 14:14:31 -0600

    Remove scripts

--- scripts/npm/build.ts
+++
@@ -1,5 +0,0 @@
-import { execFromRoot } from "../tools/root.ts";
-
-export default function build(): void {
-  execFromRoot("tsc --build tsconfig.build.json --force");
-}
--- scripts/npm/spellcheck.ts
+++
@@ -1,5 +0,0 @@
-import { execFromRoot } from "../tools/root.ts";
-
-export default function spellcheck(): void {
-  execFromRoot("npx cspell . --no-progress");
-}
