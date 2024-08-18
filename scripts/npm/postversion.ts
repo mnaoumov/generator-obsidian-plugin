9a35d4822a3f63d36145ae92fa2c3a9128ad121f
Michael Naumov <mnaoumov@gmail.com>
2024-08-17 20:10:32 -0600

    Remove old scripts

--- scripts/npm/postversion.ts
+++
@@ -1,7 +0,0 @@
-import { execFromRoot } from "../tools/root.ts";
-
-export default function postversion(): void {
-  execFromRoot("git push");
-  execFromRoot("git push --tags");
-  execFromRoot("npm publish");
-}
