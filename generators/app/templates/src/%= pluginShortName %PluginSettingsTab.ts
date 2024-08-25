e17fa3905ff38df98604360ecd4b533fd51955db
Michael Naumov <mnaoumov@gmail.com>
2024-08-25 16:21:47 -0600

    Update template

--- generators/app/templates/src/%= pluginShortName %PluginSettingsTab.ts
+++ generators/app/templates/src/%= pluginShortName %PluginSettingsTab.ts
@@ -2,17 +2,17 @@ import type <%= pluginShortName %>Plugin from "./<%= pluginShortName %>Plugin.ts
 import { PluginSettingsTabBase } from "obsidian-dev-utils/obsidian/Plugin/PluginSettingsTabBase";
 import type <%= pluginShortName %>PluginSettings from "./<%= pluginShortName %>PluginSettings.ts";
 import { Setting } from "obsidian";
+import { bindUiComponent } from "obsidian-dev-utils/obsidian/Plugin/UIComponent";
 
-export default class <%= pluginShortName %>PluginSettingsTab extends PluginSettingsTabBase<<%= pluginShortName %>Plugin, <%= pluginShortName %>PluginSettings> {
+export default class <%= pluginShortName %>PluginSettingsTab extends PluginSettingsTabBase<<%= pluginShortName %>Plugin> {
   public override display(): void {
     this.containerEl.empty();
 
     new Setting(this.containerEl)
       .setName("Test Setting")
       .setDesc("This is a test setting.")
-      .addText((text) => {
-        text.setPlaceholder("Enter a value");
-        this.bindValueComponent(text, "testSetting");
-      });
+      .addText((text) => bindUiComponent(this.plugin, text, "testSetting")
+        .setPlaceholder("Enter a value")
+      );
   }
 }
