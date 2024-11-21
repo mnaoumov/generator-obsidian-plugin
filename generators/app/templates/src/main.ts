d6007b7b47712c453088002488b32b024c6281f2
Michael Naumov <mnaoumov@gmail.com>
2024-11-20 23:42:21 -0600

    Lint to allow default export once

--- generators/app/templates/src/main.ts
+++ generators/app/templates/src/main.ts
@@ -1,3 +1,4 @@
 import { <%= pluginShortName %>Plugin } from './<%= pluginShortName %>Plugin.ts';
 
+// eslint-disable-next-line import-x/no-default-export
 export default <%= pluginShortName %>Plugin;
