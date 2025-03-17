17f712afabdbffcaa876c0f4bec3afa3b5a838ec
Michael Naumov <mnaoumov@gmail.com>
2025-03-17 12:41:58 -0600

    Use script invoke pattern

--- scripts/version.ts
+++ scripts/version.ts
@@ -1,12 +1,8 @@
-import { wrapCliTask } from 'obsidian-dev-utils/ScriptUtils/CliUtils';
-import { process } from 'obsidian-dev-utils/ScriptUtils/NodeModules';
 import { publish } from 'obsidian-dev-utils/ScriptUtils/NpmPublish';
 import { updateVersion } from 'obsidian-dev-utils/ScriptUtils/version';
 
-await wrapCliTask(async () => {
-  const VERSION_UPDATE_TYPE_ARG_INDEX = 2;
-  const versionUpdateTypeStr = process.argv[VERSION_UPDATE_TYPE_ARG_INDEX];
-  await updateVersion(versionUpdateTypeStr);
-  const isBeta = versionUpdateTypeStr === 'beta';
+export async function invoke(versionUpdateType: string): Promise<void> {
+  await updateVersion(versionUpdateType);
+  const isBeta = versionUpdateType === 'beta';
   await publish(isBeta);
-});
+}
