38320d67815ca84b873851b81b964c05b27b361b
shumadrid <195762734+shumadrid@users.noreply.github.com>
2025-03-23 20:02:11 +0100

    docs: warn against deleting the styles file import

--- generators/app/templates/src/main.ts
+++ generators/app/templates/src/main.ts
@@ -1,3 +1,4 @@
+// Do not delete this line if you want to have a styles.css file in your build output.
 import './styles/main.scss';
 import { <%= pluginShortName %>Plugin } from './<%= pluginShortName %>Plugin.ts';
 
